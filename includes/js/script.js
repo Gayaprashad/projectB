9006c082fcec3af31a813bc4ffe7149abccce5c6
unknown <trainee@PC339966.cts.com>
2016-07-13 13:32:18 +0530

    till caresol

---
+++ includes/js/script.js
@@ -0,0 +1,31 @@
+/*
+
+My Custom JS
+============
+
+Author:  Brad Hussey
+Updated: August 2013
+Notes:	 Hand coded for Udemy.com
+
+*/
+
+
+$(function(){
+    $('.carousel').carousel({
+  interval: 3000
+});
+
+
+        $('#alertMe').click(function(e){
+
+            e.preventDefault();
+
+            $('#successAlert').slideDown();
+
+        });
+
+        $('a.pop').click(funtion(e){
+            e.preventDeault();
+        });
+
+});
